d5266354265ae720834f9d79c0a627cb0cef86bc
ioaviator <kingsolomonifeanyi@gmail.com>
2024-10-15 12:09:57 +0100

    move py files to scripts folder

---
+++ scripts/load_to_csv.py
@@ -0,0 +1,15 @@
+import csv
+
+from extract import extract_data
+
+
+def load_to_csv(data, filename):
+
+    headers, rows = data
+    # Write to CSV file
+    with open(filename, "w", newline="", encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(headers)
+        writer.writerows(rows)
+
+    print("Data successfully extracted and saved to output.csv")
